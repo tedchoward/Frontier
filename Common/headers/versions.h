12fbd87a935db9f9c5fac86342b5ad3335f2ccc7
terry_teague <terry_teague@4e289971-d10f-0410-980f-25908192c0f7>
2004-11-11 09:31:39 +0000

    Bump version to v10.0a3, since v10.0a2 has already been tagged.
    git-svn-id: https://frontierkernel.svn.sourceforge.net/svnroot/frontierkernel/Frontier/trunk@183 4e289971-d10f-0410-980f-25908192c0f7

--- Common/headers/versions.h
+++ Common/headers/versions.h
@@ -54,10 +54,10 @@
 #define	frontier_subminor_version_bcd	0x00  	/* sub and minor version in BCD notation */
 
 #define frontier_stage_code				0x40	/* dev = 0x20, alpha = 0x40, beta = 0x60, final = 0x80 */
-#define	frontier_revision_level			2		/* for non-final releases only */
-#define	frontier_build_number			2		/* increment by one for every release, final or not */
+#define	frontier_revision_level			3		/* for non-final releases only */
+#define	frontier_build_number			3		/* increment by one for every release, final or not */
 
-#define frontier_version_string			"10.0a2"
+#define frontier_version_string			"10.0a3"
 
 
 #define	copyright_year_string	"2004"
