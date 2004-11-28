c49e6fab60e3c172d6e2b55ca7614628e9421e8d
andreradke <andreradke@4e289971-d10f-0410-980f-25908192c0f7>
2004-11-28 14:19:07 +0000

    Bumped version number to 10.0a4 for Frontier and Radio.
    git-svn-id: https://frontierkernel.svn.sourceforge.net/svnroot/frontierkernel/Frontier/trunk@269 4e289971-d10f-0410-980f-25908192c0f7

--- Common/headers/versions.h
+++ Common/headers/versions.h
@@ -32,18 +32,18 @@
 */ 
 
 
-#define	radio_major_version				8
-#define	radio_major_version_bcd			0x08 	/* major version in BCD notation */
+#define	radio_major_version				10
+#define	radio_major_version_bcd			0x10 	/* major version in BCD notation */
 
-#define	radio_sub_version				1
+#define	radio_sub_version				0
 #define	radio_minor_version				0
-#define	radio_subminor_version_bcd		0x10  	/* sub and minor version in BCD notation */
+#define	radio_subminor_version_bcd		0x00  	/* sub and minor version in BCD notation */
 
-#define radio_stage_code				0x60	/* dev = 0x20, alpha = 0x40, beta = 0x60, final = 0x80 */
+#define radio_stage_code				0x40	/* dev = 0x20, alpha = 0x40, beta = 0x60, final = 0x80 */
 #define	radio_revision_level			4		/* for non-final releases only */
 #define	radio_build_number				4		/* increment by one for every release, final or not */
 
-#define radio_version_string			"8.1b4"
+#define radio_version_string			"10.0a4"
 
 
 #define	frontier_major_version			10
@@ -54,10 +54,10 @@
 #define	frontier_subminor_version_bcd	0x00  	/* sub and minor version in BCD notation */
 
 #define frontier_stage_code				0x40	/* dev = 0x20, alpha = 0x40, beta = 0x60, final = 0x80 */
-#define	frontier_revision_level			3		/* for non-final releases only */
-#define	frontier_build_number			3		/* increment by one for every release, final or not */
+#define	frontier_revision_level			4		/* for non-final releases only */
+#define	frontier_build_number			4		/* increment by one for every release, final or not */
 
-#define frontier_version_string			"10.0a3"
+#define frontier_version_string			"10.0a4"
 
 
 #define	copyright_year_string	"2004"
