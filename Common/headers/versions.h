05b66f762f5fe5dfc289d71dcdd8ff3f6d0a0419
Ted C. Howard <ted@tedchoward.com>
2018-04-27 17:13:09 -0500

    Updates version to 10.2d2

--- Common/headers/versions.h
+++ Common/headers/versions.h
@@ -43,7 +43,7 @@
 /* common strings for all targets */
 
 #define	APP_COPYRIGHT_FROM	"2004"
-#define	APP_COPYRIGHT_TILL	"2015"
+#define	APP_COPYRIGHT_TILL	"2018"
 
 /* 2011-08-03 tedchoward: these properties are identical for each app */
 #define	APP_MAJOR_VERSION			10
@@ -54,10 +54,10 @@
 #define	APP_SUBMINOR_VERSION_BCD	0x20	/* sub and minor version in BCD notation */
 
 #define	APP_STAGE_CODE				0x20	/* dev = 0x20, alpha = 0x40, beta = 0x60, final = 0x80 */
-#define	APP_REVISION_LEVEL			1      /* for non-final releases only */
-#define	APP_BUILD_NUMBER			1      /* increment by one for every release, final or not */
+#define	APP_REVISION_LEVEL			2      /* for non-final releases only */
+#define	APP_BUILD_NUMBER			2      /* increment by one for every release, final or not */
 
-#define	APP_VERSION_STRING			"10.2d1"
+#define	APP_VERSION_STRING			"10.2d2"
 
 
 
